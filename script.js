5e11b13e4312e73e62f3b4e417edbfd95ca6f592
MiguelAML <miguel.ytmtz2008@gmail.com>
2026-04-22 14:37:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const text = "Ethical hacking = testing security with permission.";
+let i = 0;
 
+function typeWriter() {
+    if (i < text.length) {
+        document.getElementById("typewriter").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, 40);
+    }
+}
+
+typeWriter();
